c9595089c974ac724af8a7a5b2e8b27173f02f41
gyeolse <threewave@kakao.com>
2020-06-16 03:01:36 +0900

    gyeolse - HistoryActivity

--- app/src/main/java/com/lgj/sogyo/HistoryActivity.java
+++ app/src/main/java/com/lgj/sogyo/HistoryActivity.java
@@ -250,18 +250,4 @@ public class HistoryActivity extends AppCompatActivity implements OnMapReadyCall
 
     } //ONMAPREADY 종료 시점.
 
-    private Bitmap createDrawbleFromView(Context context, View view) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
-        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
-        view.buildDrawingCache();
-        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(bitmap);
-        view.draw(canvas);
-
-        return bitmap;
-    }
 } //HISTORY ACTIVITY 종료 시점
